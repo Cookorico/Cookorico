450db7bb12e0016b0451ec3ec13c161608ebbec6
ly4m <will.leemans@gmail.com>
2015-09-21 18:54:32 +0200

    mise en place du formatter
    Signed-off-by: ly4m <will.leemans@gmail.com>

--- src/main/java/fil/iagl/cookorico/entity/Member.java
+++ src/main/java/fil/iagl/cookorico/entity/Member.java
@@ -7,22 +7,21 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @JsonSerialize
 @Getter
 @Setter
 public class Member {
-	
-	private int id_user;
-	private String username;
-	private String firstname;
-	private String lastname;
-	private String email;
-	private Date birthday;
-	private char gender;
-	private String city;
 
-	public Member(){
-		
+	private int		id_user;
+	private String	username;
+	private String	firstname;
+	private String	lastname;
+	private String	email;
+	private Date	birthday;
+	private char	gender;
+	private String	city;
+
+	public Member() {
+
 	}
 }
