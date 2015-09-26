e765b6c1f277d8b88de748a7df074d280256402c
ly4m <will.leemans@gmail.com>
2015-09-26 15:12:47 +0200

    [Ajout security]
    Signed-off-by: ly4m <will.leemans@gmail.com>

--- src/main/java/fil/iagl/cookorico/CookoricoApplication.java
+++ src/main/java/fil/iagl/cookorico/CookoricoApplication.java
@@ -1,7 +1,5 @@
 package fil.iagl.cookorico;
 
-import java.util.List;
-
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
@@ -10,13 +8,11 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import fil.iagl.cookorico.entity.Member;
 
 
 @EnableAutoConfiguration
