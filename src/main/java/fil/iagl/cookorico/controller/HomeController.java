8be123ad7ec75af13c026ada180b75ee105eb0b2
Mambouana Mabiala Dieulin-velain <mambouanamabi@b10p19.fil.univ-lille1.fr>
2015-10-07 12:43:30 +0200

    remise du controller home en etat

--- src/main/java/fil/iagl/cookorico/controller/HomeController.java
+++ src/main/java/fil/iagl/cookorico/controller/HomeController.java
@@ -1,20 +1,5 @@
 package fil.iagl.cookorico.controller;
 
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import fil.iagl.cookorico.entity.Member;
-
-@RestController
-public class HomeController {
-	
-	/*@RequestMapping("/register")
-    public Member register(@RequestParam(value="lastname", defaultValue="World") String lastname) {
-        //return new Member(counter.incrementAndGet(), String.format(template, name));
-    }*/
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +24,4 @@ public class HomeController {
 		System.out.println(member);
 		return member;
 	}
-	
->>>>>>> origin
 }
