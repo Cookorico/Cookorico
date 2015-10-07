4bd401984ab2bd4e2c9d034f7e8a51317c29949b
Mambouana Mabiala Dieulin-velain <mambouanamabi@b10p19.fil.univ-lille1.fr>
2015-10-07 09:40:30 +0200

    register feature en cours

--- src/main/java/fil/iagl/cookorico/controller/HomeController.java
+++ src/main/java/fil/iagl/cookorico/controller/HomeController.java
@@ -1,9 +1,16 @@
 package fil.iagl.cookorico.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import fil.iagl.cookorico.entity.Member;
 
 @RestController
 public class HomeController {
-
-
+	
+	/*@RequestMapping("/register")
+    public Member register(@RequestParam(value="lastname", defaultValue="World") String lastname) {
+        //return new Member(counter.incrementAndGet(), String.format(template, name));
+    }*/
 }
