b93fcde340760163bf41014f5a39d6c8d93bd9fe
Yousfi Sofiane <yousfi@b10p18.fil.univ-lille1.fr>
2015-10-07 11:36:09 +0200

    sofianedev

--- src/main/java/fil/iagl/cookorico/service/impl/AdministratorServiceImpl.java
+++ src/main/java/fil/iagl/cookorico/service/impl/AdministratorServiceImpl.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import fil.iagl.cookorico.dao.AdministratorDao;
 import fil.iagl.cookorico.entity.Member;
+import fil.iagl.cookorico.service.AdministratorService;
 
-public class AdministratorServiceImpl {
+public class AdministratorServiceImpl implements AdministratorService{
 	
 	@Autowired
 	private AdministratorDao adminDao;
