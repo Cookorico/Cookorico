260eafac0b64cc4f045ef6cc30d0a1bc3df3713d
Philippe Antoine <antoine.philippe62@gmail.com>
2015-10-14 09:50:35 +0200

    add usr profile, tmp => need session git add src/main/resources/static/js/modules/app.js src/main/resources/static/js/modules/login.js src/main/resources/static/templates/userProfile.html !

--- src/main/resources/static/js/modules/login.js
+++ src/main/resources/static/js/modules/login.js
@@ -2,7 +2,7 @@
     
     var loginModule = angular.module('Login-module', []);
     
-    loginModule.controller('LoginController', ['$scope','$http', function ($scope, $http) {
+    loginModule.controller('LoginController', ['$scope','$http','$location',  function ($scope, $http, $location) {
     	var user;
     	this.login = function () {
         	user = angular.toJson($scope.user);
@@ -14,7 +14,9 @@
         		data : user
         	}).success(function(data, status, header, config){
         		console.log(data, status, header, config);
-        	}).error(function(data, status, header, config){
+        	       $location.search(data);
+        	       $location.path("/profile");      	
+            }).error(function(data, status, header, config){
         		console.log(data, status, header, config);
         	});
     	
@@ -27,5 +29,23 @@
         };
     
     
+    }]);
+    
+    
+    loginModule.controller('ProfileController', ['$scope', '$http', '$location', function($scope, $http, $location){
+        var queryString = $location.search();
+        if (queryString) {
+        	console.log(queryString)
+        	$scope.user = queryString;
+        }
+    	/*$http({
+    		method: 'GET', 
+    		url : '/user/:username',
+    	}).success(function(data, status, header, config){
+    		console.log(data, status, header, config);
+    		
+    	}).error(function(data, status, header, config){
+    		console.log(data, status, header, config);
+    	});*/
     }]);
  
