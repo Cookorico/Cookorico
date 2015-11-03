811878b572746db6fae0bfdc2bd4ac3eff09df70
Ouamar Sais <ouamar.sais@gmail.com>
2015-11-03 23:59:18 +0100

    utilisation controller profile

--- src/main/resources/static/js/profile/profile.js
+++ src/main/resources/static/js/profile/profile.js
@@ -1,7 +1,17 @@
 angular.module('profile', []).controller('profile',
-		function($scope){
+		function($scope, $http){
 
+		$scope.user={
+				pseudo: "Max",
+				name :"Maxime",
+				gender: "Masculin",
+				email: "max@gmail.com",
+				birthday:"15/12/1993",
+				experience: "1235",
+				level: "3"
+		};
 
+		
 
 		}
 
