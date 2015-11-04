c970c81429f9a5e06a28d719a1871a4dfd91b6c1
dufaux <johan.dufaux@etudiant.univ-lille1.fr>
2015-11-04 12:13:49 +0100

    add modal taste

--- src/main/resources/static/js/taste/taste.js
+++ src/main/resources/static/js/taste/taste.js
@@ -10,3 +10,44 @@
 angular.module('taste').controller('maprogressionCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
 }]);
+
+angular.module('taste').controller('ModalAddTasteCtrl', function ($scope, $modal, $log) {
+
+	$scope.items = ['item1', 'item2', 'item3'];
+
+	$scope.open = function (size) {
+
+		var modalInstance = $modal.open({
+			templateUrl: 'myModalContent.html',
+			controller: 'ModalInstanceCtrl',
+			size: size,
+			resolve: {
+				items: function () {
+					return $scope.items;
+				}
+			}
+		});
+
+		modalInstance.result.then(function (selectedItem) {
+			$scope.selected = selectedItem;
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+	};
+});
+
+angular.module('taste').controller('ModalAddTasteInstanceCtrl', function ($scope, $modalInstance, items) {
+
+	$scope.items = items;
+	$scope.selected = {
+			item: $scope.items[0]
+	};
+
+	$scope.ok = function () {
+		$modalInstance.close($scope.selected.item);
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+});
