1d80ec2a591358293e695bc96b4ccb55e511e6b3
unknown <dmamboua@LFR011015.corp.capgemini.com>
2015-11-04 13:51:00 +0100

    modification id et pwd bdd

--- src/main/java/fil/iagl/cookorico/CookoricoApplication.java
+++ src/main/java/fil/iagl/cookorico/CookoricoApplication.java
@@ -60,11 +60,11 @@ public class CookoricoApplication {
     public DataSource dataSource() {
 
         BasicDataSource ds = new BasicDataSource();
-		ds.setUsername("prostgres");
-		ds.setPassword("91diev12");
-		//ds.setUrl("jdbc:postgresql://172.28.1.104:5432/cookoricodb");
+		ds.setUsername("cookorico");
+		ds.setPassword("cookorico");
+		ds.setUrl("jdbc:postgresql://172.28.1.104:5432/cookoricodb");
         // url to dev at home
-        ds.setUrl("jdbc:postgresql://localhost:5432/postgres");
+//        ds.setUrl("jdbc:postgresql://localhost:5432/postgres");
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setMaxWait(25);
         return ds;
