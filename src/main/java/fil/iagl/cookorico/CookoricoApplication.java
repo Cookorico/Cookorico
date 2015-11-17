e87b476b5bf480c97ec8db514b61bb3df4a8487d
dufaux <johan.dufaux@etudiant.univ-lille1.fr>
2015-11-17 18:16:07 +0100

    add connection unencrypted

--- src/main/java/fil/iagl/cookorico/CookoricoApplication.java
+++ src/main/java/fil/iagl/cookorico/CookoricoApplication.java
@@ -63,13 +63,13 @@ public class CookoricoApplication {
     public DataSource dataSource() {
 
         BasicDataSource ds = new BasicDataSource();
-//		ds.setUsername("cookorico");
-//		ds.setPassword("cookorico");
-//		ds.setUrl("jdbc:postgresql://172.28.1.104:5432/cookoricodb");
+		ds.setUsername("cookorico");
+		ds.setPassword("cookorico");
+		ds.setUrl("jdbc:postgresql://172.28.1.104:5432/cookoricodb");
         // url to dev at home
-		ds.setUsername("postgres");
-		ds.setPassword("postgres");
-        ds.setUrl("jdbc:postgresql://localhost:5432/postgres");
+//		ds.setUsername("postgres");
+//		ds.setPassword("postgres");
+//        ds.setUrl("jdbc:postgresql://localhost:5432/postgres");
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setMaxWait(25);
         return ds;
@@ -126,7 +126,7 @@ public class CookoricoApplication {
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+            auth.userDetailsService(userDetailsService);
         }
 
         private Filter csrfHeaderFilter() {
