98b9b16c6d3be5d0c6f367c7a1854c9714a46948
Ouamar Sais <ouamar.sais@gmail.com>
2015-11-17 23:32:30 +0100

    page profile/level

--- src/main/resources/static/js/profile/profile.js
+++ src/main/resources/static/js/profile/profile.js
@@ -5,12 +5,24 @@ angular.module('profile', ['angular.css.injector', 'auth'])
 .controller('profileCtrl', function ($scope, $http, auth, cssInjector) {
 	
 	$scope.user= {};
-
+	$scope.level ={};
+	
+	//Get user details
 	$http({
 		method: 'GET', 
-		url : '/profile'
+		url : '/user'
 	}).success(function(data, status, header, config){
-		$scope.user = data;
+		$scope.user = data.principal.member;
+	}).error(function(data, status, header, config){
+		console.log(data, status, header, config);
+	});
+	
+	//Get user level
+	$http({
+		method: 'GET', 
+		url : '/level/xp/{{scope.user.experience}}'
+	}).success(function(data, status, header, config){
+		$scope.level = data;
 	}).error(function(data, status, header, config){
 		console.log(data, status, header, config);
 	});
