fbd647b7640beb6333f9cdff6242a576b853508d
Dieulin <mambouana.dieulin@sfr.fr>
2015-12-01 03:34:49 +0100

    ajout recette : gestion des flash messages terminée

--- src/main/resources/static/js/recipe/recipe.js
+++ src/main/resources/static/js/recipe/recipe.js
@@ -1,4 +1,4 @@
-angular.module('recipe', [])
+angular.module('recipe', ['flash', 'ngAnimate'])
 .controller('listingRecipeCtrl', function($scope, $http) {
     $http.get('/recipe/list')
     .success(function(data, status, headers, config) {
@@ -15,12 +15,13 @@ angular.module('recipe', [])
     .error(function(data, status, headers, config) {
         // log error
     });
-}).controller('addRecipeCtrl', function ($scope, $location, $http) {
+}).controller('addRecipeCtrl', ['$rootScope', '$scope', 'Flash', '$location', '$http',  function ($rootScope, $scope, Flash, $location, $http) {
 	
 	$scope.add = function () {
 		
-		var recipe = angular.toJson($scope.recipe);		
-		console.log($scope.recipe);		
+		var recipe = angular.toJson($scope.recipe);
+		console.log($scope.recipe);
+		console.log(recipe);
 		
 		// send recipe to the recipe controller
 		$http({
@@ -29,11 +30,12 @@ angular.module('recipe', [])
     		data : recipe
     	})
     	.success(function(data, status, header, config){
-    		$location.path('/recipe');
-    		//$flash.create('success', 'Recette crée', 'custom-class');
+    		Flash.create('success', 'Votre nouvelle recette a été bien ajoutée !');
+    		$location.path('/dashboard/recipe');
     	})
     	.error(function(data, status, header, config){
-    		//console.log(data, status, header, config);
+    		Flash.create('danger', 'Suite à une erreur votre recette na pas été sauvegardée');
+    		$location.path('/dashboard/home');
     	});
     };
-});
+}]);
