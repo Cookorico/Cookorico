0687ffbde6b71ca038bf8c3bf40471bcf9430a8e
Dieulin <mambouana.dieulin@sfr.fr>
2016-01-03 18:30:07 +0100

    correction bugs amélioration interface

--- src/main/java/fil/iagl/cookorico/controller/RecipeController.java
+++ src/main/java/fil/iagl/cookorico/controller/RecipeController.java
@@ -28,115 +28,115 @@ import fil.iagl.cookorico.service.RecipeService;
 
 @RestController
 public class RecipeController {
-	
-	@Autowired
-	private PictureService pictureServive;
-	
-	@Autowired
-	private RecipeService recipeService;
-	
-	@Autowired
-	AdministratorService administratorService;
-	
-	/**
-	 * Recherche une recette par son id
-	 * @param id L'id de la recette
-	 * @return La recette trouvée
-	 */
-	@RequestMapping(value="/recipe/{id}", method = RequestMethod.GET)
-	public @ResponseBody Recipe getRecipe(@PathVariable String id) {
-		return recipeService.getRecipeById(Integer.parseInt(id));
-	}
-	
-	/**
-	 * Ajout en vitesse le 02/12, nom à check, utilisé dans recipectrl.
-	 * @param id Id de la recette
-	 * @return True si le user courrent est le créateur de la recette
-	 */
-	@RequestMapping(value="/recipe/{id}/currentUserIsCreator", method = RequestMethod.GET)
-	public @ResponseBody Boolean currentUserIsCreator(@PathVariable String id) {
-		
-		Recipe recipe = recipeService.getRecipeById(Integer.parseInt(id));
-		CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	    Member m = currentUser.getMember();
-	    
-	    return m.getIdMember() == recipe.getCreator().getIdMember();
-	}
-	
-	/**
-	 * Recuperation des recettes
-	 * @param mainpic Si à true alors on recupère aussi les photos de la recette
-	 * @param tags Si à true alors on recupère aussi les tags de la recette
-	 * @return liste des recettes trouvées
-	 */
-	@RequestMapping(value="/recipes", method = RequestMethod.GET)
-	public @ResponseBody List<Recipe> getListRecipe(@RequestParam(value = "mainpic", required = false) boolean mainpic, @RequestParam(value = "tags", required = false) boolean tags) {
-		return recipeService.getAllRecipes(mainpic, tags);
-	}
-	
-	/**
-	 * Ajout recette dans la BDD
-	 * @param model infos recette clé-valeur
-	 * @return Recette créée
-	 */
-	@RequestMapping(value = "/recipe/add", method = RequestMethod.POST)
-	public @ResponseBody Recipe addRecipe(@RequestBody ModelMap model){
-		
-		// get recipe form data
-		int preparationTime = Integer.valueOf(String.valueOf(model.get("rcp_preparation_time")));
-		int cookingTime = Integer.valueOf(String.valueOf(model.get("rcp_cooking_time")));
-		int difficulty = Integer.valueOf(String.valueOf(model.get("rcp_difficulty")));
-		int experienceVal = Integer.valueOf(String.valueOf(model.get("rcp_experienceVal")));
-		String description = String.valueOf(model.get("rcp_description"));
-		String name = String.valueOf(model.get("rcp_name"));
-		String dish_type = String.valueOf(model.get("rcp_dish_type"));
-		//int mainPictureId = Integer.valueOf(String.valueOf(model.get("mainPictureId")));
-		
-		Date date = new Date();
-		Timestamp creationDate = new Timestamp(date.getTime());
-		CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	    Member creator = currentUser.getMember();
-	    
-	    // create recipe entity
-		Recipe recipe = new Recipe();
-		recipe.setName(name);
-		recipe.setDescription(description);
-		recipe.setPreparationTime(preparationTime);
-		recipe.setCookingTime(cookingTime);
-		recipe.setCreator(creator);
-		recipe.setDishType(dish_type);
-		recipe.setDifficulty(difficulty);
-		recipe.setDraft(false); // TODO : false est valeur par défaut ?
-		//recipe.setMainPicture(this.pictureServive.getPictureById(mainPictureId)); // TODO : verifier mapper Picture pour getPictureById
-		recipe.setCreationDate(creationDate);
-		recipe.setModifDate(creationDate);
-		recipe.setValidation(false);
-		// TODO : recipe.SetValidator(integer)
-		recipe.setDisabled(false);
-		recipe.setExperienceVal(experienceVal);
-		
-		/*
-		// debug
-		System.out.println("name = " + recipe.getName());
-		System.out.println("description = " + recipe.getDescription());
-		System.out.println("preparationTime = " + recipe.getPreparationTime());
-		System.out.println("cookingTime = " + recipe.getCookingTime());
-		System.out.println("creator id = " + recipe.getCreator().getIdMember());
-		System.out.println("dishType = " + recipe.getDishType());
-		System.out.println("difficulty = " + recipe.getDifficulty());
-		System.out.println("creationDate = " + recipe.getCreationDate());
-		System.out.println("modifDate = " + recipe.getModifDate());
-		System.out.println("validation = " + recipe.getValidation());
-		System.out.println("disabled = " + recipe.getDisabled());
-		System.out.println("experienceVal = " + recipe.getExperienceVal());
-		System.out.println("front : mainPicture id = " + mainPictureId);
-		System.out.println("call service = " + this.pictureServive.getPictureById(mainPictureId).getIdPicture());
-		System.out.println("back : mainPicture id = " + recipe.getMainPicture().getIdPicture());
-		*/
-		
-		// save the recipe to bdd
-		this.recipeService.addRecipe(recipe);
-		
-		return recipe;
-	}	
+    
+    @Autowired
+    private PictureService pictureServive;
+    
+    @Autowired
+    private RecipeService recipeService;
+    
+    @Autowired
+    AdministratorService administratorService;
+    
+    /**
+     * Recherche une recette par son id
+     * @param id L'id de la recette
+     * @return La recette trouvée
+     */
+    @RequestMapping(value="/recipe/{id}", method = RequestMethod.GET)
+    public @ResponseBody Recipe getRecipe(@PathVariable String id) {
+        return recipeService.getRecipeById(Integer.parseInt(id));
+    }
+    
+    /**
+     * Ajout en vitesse le 02/12, nom à check, utilisé dans recipectrl.
+     * @param id Id de la recette
+     * @return True si le user courrent est le créateur de la recette
+     */
+    @RequestMapping(value="/recipe/{id}/currentUserIsCreator", method = RequestMethod.GET)
+    public @ResponseBody Boolean currentUserIsCreator(@PathVariable String id) {
+        
+        Recipe recipe = recipeService.getRecipeById(Integer.parseInt(id));
+        CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Member m = currentUser.getMember();
+        
+        return m.getIdMember() == recipe.getCreator().getIdMember();
+    }
+    
+    /**
+     * Recuperation des recettes
+     * @param mainpic Si à true alors on recupère aussi les photos de la recette
+     * @param tags Si à true alors on recupère aussi les tags de la recette
+     * @return liste des recettes trouvées
+     */
+    @RequestMapping(value="/recipes", method = RequestMethod.GET)
+    public @ResponseBody List<Recipe> getListRecipe(@RequestParam(value = "mainpic", required = false) boolean mainpic, @RequestParam(value = "tags", required = false) boolean tags) {
+        return recipeService.getAllRecipes(mainpic, tags);
+    }
+    
+    /**
+     * Ajout recette dans la BDD
+     * @param model infos recette clé-valeur
+     * @return Recette créée
+     */
+    @RequestMapping(value = "/recipe/add", method = RequestMethod.POST)
+    public @ResponseBody Recipe addRecipe(@RequestBody ModelMap model){
+        
+        // get recipe form data
+        int preparationTime = Integer.valueOf(String.valueOf(model.get("rcp_preparation_time")));
+        int cookingTime = Integer.valueOf(String.valueOf(model.get("rcp_cooking_time")));
+        int difficulty = Integer.valueOf(String.valueOf(model.get("rcp_difficulty")));
+        int experienceVal = Integer.valueOf(String.valueOf(model.get("rcp_experienceVal")));
+        String description = String.valueOf(model.get("rcp_description"));
+        String name = String.valueOf(model.get("rcp_name"));
+        String dish_type = String.valueOf(model.get("rcp_dish_type"));
+        //int mainPictureId = Integer.valueOf(String.valueOf(model.get("mainPictureId")));
+        
+        Date date = new Date();
+        Timestamp creationDate = new Timestamp(date.getTime());
+        CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Member creator = currentUser.getMember();
+        
+        // create recipe entity
+        Recipe recipe = new Recipe();
+        recipe.setName(name);
+        recipe.setDescription(description);
+        recipe.setPreparationTime(preparationTime);
+        recipe.setCookingTime(cookingTime);
+        recipe.setCreator(creator);
+        recipe.setDishType(dish_type);
+        recipe.setDifficulty(difficulty);
+        recipe.setDraft(false); // TODO : false est valeur par défaut ?
+        //recipe.setMainPicture(this.pictureServive.getPictureById(mainPictureId)); // TODO : verifier mapper Picture pour getPictureById
+        recipe.setCreationDate(creationDate);
+        recipe.setModifDate(creationDate);
+        recipe.setValidation(false);
+        // TODO : recipe.SetValidator(integer)
+        recipe.setDisabled(false);
+        recipe.setExperienceVal(experienceVal);
+        
+        /*
+        // debug
+        System.out.println("name = " + recipe.getName());
+        System.out.println("description = " + recipe.getDescription());
+        System.out.println("preparationTime = " + recipe.getPreparationTime());
+        System.out.println("cookingTime = " + recipe.getCookingTime());
+        System.out.println("creator id = " + recipe.getCreator().getIdMember());
+        System.out.println("dishType = " + recipe.getDishType());
+        System.out.println("difficulty = " + recipe.getDifficulty());
+        System.out.println("creationDate = " + recipe.getCreationDate());
+        System.out.println("modifDate = " + recipe.getModifDate());
+        System.out.println("validation = " + recipe.getValidation());
+        System.out.println("disabled = " + recipe.getDisabled());
+        System.out.println("experienceVal = " + recipe.getExperienceVal());
+        System.out.println("front : mainPicture id = " + mainPictureId);
+        System.out.println("call service = " + this.pictureServive.getPictureById(mainPictureId).getIdPicture());
+        System.out.println("back : mainPicture id = " + recipe.getMainPicture().getIdPicture());
+        */
+        
+        // save the recipe to bdd
+        this.recipeService.addRecipe(recipe);
+        
+        return recipe;
+    }
 }
