839d7df3bfe1c60fdf3db4ff8fff902e25a0a7b1
dufaux <johan.dufaux@etudiant.univ-lille1.fr>
2016-02-10 09:42:10 +0100

    fix angular error

--- src/main/resources/static/js/cookorico.js
+++ src/main/resources/static/js/cookorico.js
@@ -1,60 +1,57 @@
+/**
+ * Created by leemans on 03/11/15.
+ */
+
 window.app_version = 2;
 
+var cookorico = angular.module('cookorico', ['ngRoute', 'auth', 'home', 'profile', 'recipe', 'producer', 'message',
+        'progression',
+        'ui.router',
+        'ngAnimate',
+        'ui.bootstrap',
+        'textAngular',
+        'ui.calendar',
+        'perfect_scrollbar',
+        'angular-loading-bar',
+        'chart.js',
+        'angular-growl',
+        'angulartics',
+        'angulartics.google.analytics',
+        'gridshore.c3js.chart',
+        'growlNotifications',
+        'angular.css.injector'
+    ])
+    .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.latencyThreshold = 5;
+        cfpLoadingBarProvider.includeSpinner = false;
+    }])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
-var cookorico = angular.module('cookorico', [
-	'ngRoute', 
-	'auth', 
-	'home', 
-	'profile', 
-	'recipe',
-	'producer',
-	'progression',
-	'message',
-	'ui.router',
-    'ngAnimate',
-    'ui.bootstrap',
-    'textAngular',
-    'ui.calendar',
-    'perfect_scrollbar',
-    'angular-loading-bar',
-    'chart.js',
-    'angular-growl',
-    'angulartics',
-    'angulartics.google.analytics',
-    'gridshore.c3js.chart',
-    'growlNotifications',
-    'angular.css.injector'
-])
-.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
-    cfpLoadingBarProvider.latencyThreshold = 5;
-    cfpLoadingBarProvider.includeSpinner = false;
-}])
-.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+        $locationProvider.html5Mode(false);
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-    $locationProvider.html5Mode(false);
-    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        $urlRouterProvider.when('/dashboard', '/dashboard/home');
+        $urlRouterProvider.otherwise('/dashboard');
 
-    $urlRouterProvider.when('/dashboard', '/dashboard/home');
-    $urlRouterProvider.otherwise('/dashboard');
+        $stateProvider
+            .state('accueil', {
+                url: '/',
+                templateUrl: 'js/navigation/login.html',
+                controller: 'navigation'
+            })
+            
+            .state('plain', {
+                abstract: true,
+                url: '',
+                templateUrl: 'views/layouts/plain.html?v=' + window.app_version,
+            })
+            .state('boxed', {
+                abstract: true,
+                url: '',
+                parent: 'plain',
+                templateUrl: 'views/layouts/boxed.html?v=' + window.app_version,
+            })
 
-    $stateProvider
-        .state('accueil', {
-            url: '/',
-            templateUrl: 'js/navigation/login.html',
-            controller: 'navigation'
-        })
-        
-        .state('plain', {
-            abstract: true,
-            url: '',
-            templateUrl: 'views/layouts/plain.html?v=' + window.app_version,
-        })
-        .state('boxed', {
-            abstract: true,
-            url: '',
-            parent: 'plain',
-            templateUrl: 'views/layouts/boxed.html?v=' + window.app_version,
-        })
             .state('login', {
                 url: '/login',
                 parent: 'boxed',
@@ -258,8 +255,7 @@ var cookorico = angular.module('cookorico', [
             .state('progression', {
                 url: '/progression',
                 parent: 'dashboard',
-                templateUrl: 'views/pages/dashboard/maprogression.html?v=' + window.app_version,
-                controller: 'maprogressionCtrl'
+                templateUrl: 'js/progression/progression.html'
             })
             .state('taste', {
             	url: '/taste',
@@ -301,25 +297,20 @@ var cookorico = angular.module('cookorico', [
                 parent: 'dashboard',
                 templateUrl: 'js/producer/producer.html',
                 controller: 'ProducerCtrl'
-            })
-            .state('progression', {
-	            url: '/progression',
-	            parent: 'dashboard',
-	            templateUrl: 'js/progression/progression.html'
             });
     })
     .run(function (auth, $rootScope) {
 
-    var switchValue = JSON.parse(localStorage.getItem("switched"));
+        var switchValue = JSON.parse(localStorage.getItem("switched"));
 
-    if (switchValue)
-        $('body').addClass('box-section');
+        if (switchValue)
+            $('body').addClass('box-section');
 
-    auth.init("/dashboard/home","/", "/logout");
+        auth.init("/dashboard/home","/", "/logout");
 
-    $rootScope.$on('$routeChangeStart', function(event, next, current) {
-        console.debug("COUCOU");
+        $rootScope.$on('$routeChangeStart', function(event, next, current) {
+            console.debug("COUCOU");
+        });
+
+
     });
-
-
-});
