4b6b8e825bb12ba821186ac83310a65b396ca0b1
Ouamar Sais <ouamar.sais@gmail.com>
2016-02-22 00:10:16 +0100

    modal producer without content

--- src/main/resources/static/js/recipe/directives/recipeDirective.js
+++ src/main/resources/static/js/recipe/directives/recipeDirective.js
@@ -1,84 +1,107 @@
 angular.module('cookorico').directive('ccRecipe', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeThumbnail.html',
-        scope: {
-            recipe: '='
-        }
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeThumbnail.html',
+		scope: {
+			recipe: '='
+		}
+	}
 });
 
 angular.module('cookorico').directive('ccRecipesHeader', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeFilter.html',
-        controller: function ($scope) {
-            $scope.typePlat = 'Plat';
-        }
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeFilter.html',
+		controller: function ($scope) {
+			$scope.typePlat = 'Plat';
+		}
+	}
 });
 
 angular.module('cookorico').directive('ccRecipeInfos', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeInfos.html'
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeInfos.html'
+	}
 });
 
 angular.module('cookorico').directive('ccRecipeMainPicture', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipePictures.html'
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipePictures.html'
+	}
 });
 
 angular.module('cookorico').directive('ccRecipeIngredients', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeIngredients.html'
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeIngredients.html'
+	}
 });
 
 angular.module('cookorico').directive('ccRecipeSteps', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeSteps.html'
-    }
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeSteps.html'
+	}
+});
+
+angular.module('cookorico').directive('modalDialog', function (){
+	return {
+		restrict: 'E',
+		scope: {
+			show: '='
+		},
+		replace: true, // Replace with the template below
+		transclude: true, // we want to insert custom content inside the directive
+		link: function(scope, element, attrs) {
+			scope.dialogStyle = {};
+			if (attrs.width)
+				scope.dialogStyle.width = attrs.width;
+			if (attrs.height)
+				scope.dialogStyle.height = attrs.height;
+			scope.hideModal = function() {
+				scope.show = false;
+			};
+		},
+		templateUrl: 'js/recipe/templates/modalProducer.html'
+	};
+
 });
 
 angular.module('cookorico').directive('ccRecipeComments', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/recipe/templates/recipeComments.html',
-        controller: function ($scope, $http, $stateParams) {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/recipe/templates/recipeComments.html',
+		controller: function ($scope, $http, $stateParams) {
 
-            refresh();
+			refresh();
 
-            $scope.saveComment = function (newComment, idRecipe) {
+			$scope.saveComment = function (newComment, idRecipe) {
 
-                $scope.newComment.recipe = {};
-                $scope.newComment.recipe.idRecipe = idRecipe;
+				$scope.newComment.recipe = {};
+				$scope.newComment.recipe.idRecipe = idRecipe;
 
-                $http({
-                    method: 'POST',
-                    url: '/comment/add',
-                    data: newComment
-                }).success(function () {
-                    refresh();
-                });
+				$http({
+					method: 'POST',
+					url: '/comment/add',
+					data: newComment
+				}).success(function () {
+					refresh();
+				});
 
-            };
+			};
 
-            function refresh() {
-                $http({
-                    method: 'GET',
-                    url: '/comments/recipe/' + $stateParams.idRecipe
-                }).success(function (response) {
-                    $scope.recipe.comments = response;
-                    $scope.newComment = {};
-                });
-            }
+			function refresh() {
+				$http({
+					method: 'GET',
+					url: '/comments/recipe/' + $stateParams.idRecipe
+				}).success(function (response) {
+					$scope.recipe.comments = response;
+					$scope.newComment = {};
+				});
+			}
 
-        }
-    }
+		}
+	}
 });
