00b3fb059c693e0bd0af529dcfe609c7670b18eb
Ly4m <will.leemans@gmail.com>
2016-02-21 16:30:32 +0100

    Big Refractor

--- src/main/resources/static/js/cookorico.js
+++ src/main/resources/static/js/cookorico.js
@@ -109,8 +109,8 @@ var cookorico = angular.module('cookorico', [
             .state('newrecipe', {
                 url: '/newrecipe',
                 parent: 'dashboard',
-                templateUrl: 'js/recipe/newrecipe.html',
-                controller: 'addRecipeCtrl'
+                templateUrl: 'js/recipe/templates/recipeCreate.html',
+                controller: 'recipeCreateCtrl'
             })
             .state('recipe', {
                 url: '/recipe/:idRecipe',
---
+++ src/main/resources/static/js/recipe/controllers/recipeCreateController.js
@@ -0,0 +1,8 @@
+angular.module('cookorico').controller('recipeCreateCtrl', function ($scope, $http, $stateParams) {
+
+    $scope.recipe = {
+        dishType: 'Plat',
+        difficulty: 5
+    }
+
+});
