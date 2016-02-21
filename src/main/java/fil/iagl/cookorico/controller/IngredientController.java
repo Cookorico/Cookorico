9d224b09ca8edd8f66af1c81ae89d19c851d48f9
Ly4m <will.leemans@gmail.com>
2016-02-21 19:04:33 +0100

    Ajout ingrédient a recette

--- src/main/java/fil/iagl/cookorico/controller/IngredientController.java
+++ src/main/java/fil/iagl/cookorico/controller/IngredientController.java
@@ -1,69 +1,66 @@
 package fil.iagl.cookorico.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import fil.iagl.cookorico.entity.Ingredient;
 import fil.iagl.cookorico.service.IngredientInRecipeService;
 import fil.iagl.cookorico.service.IngredientService;
 import fil.iagl.cookorico.wrapper.IngredientWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class IngredientController {
 
-	
-	@Autowired
-	IngredientService ingredientService;
-	
-	@Autowired
-	IngredientInRecipeService ingredientInRecipeService;
-	
-	
-	
-	@RequestMapping(value="/measurements", method = RequestMethod.GET)
-	public @ResponseBody List<String> getAllMeasurement() {
-		return ingredientInRecipeService.getAllMeasurements();
-	}
-	
-	
-	
-	@RequestMapping(value="/ingredients", method = RequestMethod.GET)
-	public @ResponseBody List<Ingredient> getAllIngredients(
-			@RequestParam(value = "mainpic", required = false) boolean mainpic, 
-			@RequestParam(value = "tags", required = false) boolean tags) {
-		return ingredientService.getAllIngredients(mainpic, tags);
-	}
-	
-	
-	@RequestMapping(value="/ingredient/list", method = RequestMethod.POST)
-	public boolean addIngredientToList(@RequestBody IngredientWrapper wrapper){
-		final String name = wrapper.getName();
-		final String description = wrapper.getDescription();
-		
-		final Ingredient ingredient = new Ingredient();
-		ingredient.setName(name);
-		ingredient.setDescription(description);
-		
-		return ingredientService.addIngredient(ingredient);
-		
-	}
-	@RequestMapping(value="/ingredient/delete", method = RequestMethod.DELETE)
-	public boolean deleteIngredient(@RequestBody IngredientWrapper wrapper){
-		final String name = wrapper.getName();
-		
-		final Ingredient ingredient = new Ingredient();
-		
-		ingredient.setName(name);
-		
-		return ingredientService.deleteIngredient(ingredient);
-	}
-		
-	
+
+    @Autowired
+    IngredientService ingredientService;
+
+    @Autowired
+    IngredientInRecipeService ingredientInRecipeService;
+
+
+    @RequestMapping(value = "/measurements", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<String> getAllMeasurement() {
+        return ingredientInRecipeService.getAllMeasurements();
+    }
+
+
+    @RequestMapping(value = "/ingredients", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<Ingredient> getAllIngredients(
+            @RequestParam(value = "mainpic", required = false) boolean mainpic,
+            @RequestParam(value = "tags", required = false) boolean tags) {
+        return ingredientService.getAllIngredients(mainpic, tags);
+    }
+
+
+    @RequestMapping(value = "/ingredient/list", method = RequestMethod.POST)
+    public boolean addIngredientToList(@RequestBody IngredientWrapper wrapper) {
+        final String name = wrapper.getName();
+        final String description = wrapper.getDescription();
+
+        final Ingredient ingredient = new Ingredient();
+        ingredient.setName(name);
+        ingredient.setDescription(description);
+
+        return ingredientService.addIngredient(ingredient);
+
+    }
+
+    @RequestMapping(value = "/ingredient/delete", method = RequestMethod.DELETE)
+    public boolean deleteIngredient(@RequestBody IngredientWrapper wrapper) {
+        final String name = wrapper.getName();
+
+        final Ingredient ingredient = new Ingredient();
+
+        ingredient.setName(name);
+
+        return ingredientService.deleteIngredient(ingredient);
+    }
+
+
 }
--- src/main/resources/static/js/recipe/controllers/recipeCreateController.js
+++ src/main/resources/static/js/recipe/controllers/recipeCreateController.js
@@ -1,16 +1,34 @@
 angular.module('cookorico').controller('recipeCreateCtrl', function ($scope, $http, $stateParams) {
 
+    $scope.ingredientInRecipe = {
+        ingredient: {}
+    };
 
     $http({
         method: 'GET',
         url: '/ingredients'
-    }).then(function successCallback(response) {
+    }).success(function successCallback(response) {
         $scope.ingredients = response;
+    });
+
+    $http({
+        method: 'GET',
+        url: '/measurements'
+    }).then(function successCallback(response) {
+        $scope.measurements = response.data;
     });
 
     $scope.recipe = {
         dishType: 'Plat',
-        difficulty: 5
-    }
+        difficulty: 5,
+        ingredients: []
+    };
 
+    $scope.addIngredient = function () {
+        $scope.recipe.ingredients.push($scope.ingredientInRecipe);
+
+        $scope.ingredientInRecipe = {
+            ingredient: {}
+        };
+    }
 });
