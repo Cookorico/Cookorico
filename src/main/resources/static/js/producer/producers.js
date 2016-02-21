bb95cfea0abeb420eea5f450168d802d318010b0
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2016-02-21 23:27:44 +0100

    page prod, affichage infos prod + début ajout produit

--- src/main/resources/static/js/producer/producers.js
+++ src/main/resources/static/js/producer/producers.js
@@ -5,12 +5,13 @@ var producerModule = angular.module('producer', ['flash', 'ngAnimate', 'ngFileUp
 
 
 /**
- * Controller to show recipes list
+ * Controller to show producers list
  */
 producerModule.controller('ProducersCtrl', function($scope, $http) {
     
     $http.get('/producers').success(function(data, status, headers, config) {
         $scope.producers = data;
+        console.log(data);
     }).error(function(data, status, headers, config) {
         console.log(data);
     });
@@ -18,18 +19,45 @@ producerModule.controller('ProducersCtrl', function($scope, $http) {
 
 
 /**
- * Controller to show recipes list
+ * Controller to show one producer
  */
 producerModule.controller('ProducerCtrl',  ['$scope','$stateParams','$http', '$rootScope', function ($scope, $stateParams, $http, $rootScope, auth, cssInjector) {
     
+	
+	var ingredients = [{'name':'carottes'}, {'name':'oeuf'}]
+	$scope.producer = {'name':'Antoine', 'city':'Lille', 'description': 'blablalblabla', 'ingredients':ingredients}
+	$scope.listIngre
+	
+	$scope.inputIngredient = '';
+	
+
 	//get main infos
     $http({
         method: 'GET', 
         url : '/producer/'+$stateParams.idProducer
     }).then(function successCallback(response) {
-        $scope.producer = response.data;
+        //$scope.producer = response.data;
     }, function errorCallback(response) {
         console.error(data, status, header, config);
     });
     
+    $http({
+	    method: 'GET',
+	    url: '/ingredients?mainpic=true'
+	  }).success(function (data, status, headers, config) {
+		    $scope.ingredients = data;
+	  })
+	  .error(function (data, status, headers, config) {
+	  });
+    
+    
+	$scope.findIngredient = function(ingredient){
+//		if(ingredient.name.toLowerCase().match($scope.inputIngredient.toLowerCase()))
+//			console.log(ingredient.name.toLowerCase().match($scope.inputIngredient.toLowerCase()));
+//		console.log($scope.inputIngredient.toLowerCase());
+		return ingredient.name.toLowerCase().match($scope.inputIngredient.toLowerCase()) 
+		&& $scope.inputIngredient.length >= 2 
+		&& $scope.ingredients.indexOf(ingredient.name.toLowerCase()) == -1; 
+	}
+	
 }]);
